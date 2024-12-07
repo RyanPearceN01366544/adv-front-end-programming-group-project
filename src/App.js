738afca281d7f68000dc97183c9e27f6fac9677d
n01541512 <145499937+n01541512@users.noreply.github.com>
2024-12-06 22:24:50 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './Navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello! :)
-        </p>
-      </header>
+      <Navigation/>
     </div>
   );
 }
 
 export default App;
+
